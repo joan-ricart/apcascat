f4ed04144c87a610994d8b436a9c750b24335283
Joan <j.ricartt@gmail.com>
2025-10-11 11:02:51 +0200

    dev env fixes

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 ## APCASCAT
-
-To spin up the development environment, ensure [podman](https://podman.io) is installed and running.
-
-Modify run_pod.sh's variables to your liking, then simply execute ./run_pod.sh from the project root.
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    server: {
+        host: "0.0.0.0",       // listen inside Docker container
+        port: 5173,
+        hmr: {
+            host: "localhost", // the host the browser will connect to
+        },
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
